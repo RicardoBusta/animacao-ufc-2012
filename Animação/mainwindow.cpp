38e736f79992a3ec0e36c6d6a17e80547fca5049
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@fc5625e7-3c55-33a6-85dd-dd15f2f84649>
2012-12-11 14:31:27 +0000

    funciona

--- Animação/mainwindow.cpp
+++ Animação/mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    animation = new Animation(this);
+    ui->glFrame->animation = animation;
+
+    connect(ui->play,SIGNAL(clicked()),animation,SLOT(play()));
+    connect(ui->stop,SIGNAL(clicked()),animation,SLOT(stop()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::updateFrame()
+{
+    ui->horizontalSlider->setMaximum(animation->frame.size());
+    ui->horizontalSlider->setValue(animation->currentFrame);
+    ui->glFrame->update();
 }
